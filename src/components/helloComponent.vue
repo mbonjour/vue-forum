9a045678e41d7eea34ef39dac6caa855178b794a
mbonjour <mic.bonjour@hotmail.fr>
2017-03-06 22:51:30 +0100

    premiers essais pas du tout concluant

---
+++ src/components/helloComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="helloComponent">
+    <ul>
+      <li v-for="post in posts">{{ post.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'helloComponent',
+  data () {
+    return {
+      posts: []
+    }
+  },
+  created: function () {
+    this.$http.get('http://jsonplaceholder.typicode.com/posts', (data) => {
+      this.posts = data
+      console.log(data)
+    })
+    .error((err) => console.log(err))
+  }
+}
+</script>
